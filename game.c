c85db5014aa456bd9dc4561fc788ad49c07244a4
Renfer, Florian <florian.renfer@heyoptima.de>
2025-09-15 21:27:53 +0200

    feat: add fps logging, disable v-sync, toggle outlines

--- game.c
+++ game.c
@@ -30,21 +30,72 @@ const char *fragment_shader_src = "#version 330 core\n"
                                   "   FragColor = vec4(color, 1.0);\n"
                                   "}\n";
 
-float vertices[] = {-0.05f, -0.05f, 0.05f, -0.05f, 0.05f, 0.05f, -0.05f, 0.05f};
-unsigned int indices[] = {0, 1, 2, 2, 3, 0};
+// clang-format off
+float vertices[] = {
+  0.00f, 0.05f,
+  -0.025f, -0.05f,
+  0.00f, 0.00f,
+  0.025f, -0.05f,
+};
+
+unsigned int indices[] = {
+  0, 1, 2,
+  2, 3, 0
+};
+// clang-format on
+
+unsigned int currentRenderingMode = GL_FILL;
+
+unsigned int fps = 0;
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
+}
+
+/**
+ * Processes input events received by the window.
+ * - Pressing the ESC key will close the window.
+ *   - Pressing the T key will toggle between wireframe and fill rendering
+ * modes.
+ */
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, 1);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS) {
+    switch (currentRenderingMode) {
+    case GL_LINE:
+      currentRenderingMode = GL_FILL;
+      break;
+    case GL_FILL:
+      currentRenderingMode = GL_LINE;
+      break;
+    }
+    glPolygonMode(GL_FRONT_AND_BACK, currentRenderingMode);
+  }
 }
 
 float randf(float min, float max) {
   return min + (float)rand() / ((float)RAND_MAX / (max - min));
 }
 
+void calculateFps(int *prev) {
+  int curr = glfwGetTime();
+  fps++;
+
+  if (curr - *prev >= 1) {
+    fprintf(stdout, "FPS:%d\n", fps);
+    fps = 0;
+    *prev = curr;
+  }
+}
+
 int main() {
   srand((unsigned int)time(NULL));
   if (!glfwInit())
     return EXIT_FAILURE;
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -62,15 +113,15 @@ int main() {
   glfwMakeContextCurrent(window);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // Disable v-sync (v-sync limits the FPS to the monitor refresh rate)
+  glfwSwapInterval(0);
 
   if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
     fprintf(stderr, "Error getting process address.\n");
     return EXIT_FAILURE;
   }
+
+  int prevtime = glfwGetTime();
 
   // Compile shaders
   unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
@@ -117,10 +168,15 @@ int main() {
   int colorLoc = glGetUniformLocation(shader, "color");
 
   while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    calculateFps(&prevtime);
+
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     for (int i = 0; i < NUM_SHAPES; ++i) {
+      // Not really collision detection
       shapes[i].x += shapes[i].dx;
       shapes[i].y += shapes[i].dy;
       if (fabsf(shapes[i].x) > 0.95f)
@@ -130,6 +186,7 @@ int main() {
 
       glUniform2f(offsetLoc, shapes[i].x, shapes[i].y);
       glUniform3f(colorLoc, shapes[i].r, shapes[i].g, shapes[i].b);
+
       glBindVertexArray(VAO);
       glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     }
